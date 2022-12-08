1dc40e5268150d8560a3e4f82024f3df51756570
Chanab0910 <chanab0910@highgateschool.org.uk>
2022-12-08 10:07:41 +0000

    initial commit

--- CLI.py
+++ CLI.py
@@ -1,13 +1,63 @@
-from game_object import Game, PlayerObject, ComputerPlayer
+from game_object import PlayerObject, HumanPlayer, ComputerPlayer, Game
 
 
-# Command Line Interface - gives prompts to run the game from the Command line
-class ClInterface:
+class Cli_interface:
     def __init__(self):
         self.game = Game()
+        self.name = None
 
     def set_up(self):
-        print('welcome to rock-paper-scissors')
-        self.player1 = input('do you want player 1 to be a human or computer')
+        print("Welcome to Rock-Paper-Scissors-Lizard-Spock")
+        num_players = int(input("Enter how many players you would like to add: "))
+        while num_players != 1 and num_players != 2:
+            num_players = int(input("Enter a number of players that is 1 or 2: "))
+
+        if num_players == 1:
+            name = input("Please enter your name: ")
+            self.game.add_human_player(name)
+            self.game.add_computer_player()
+        elif num_players == 2:
+            name1 = input("Please enter your name for Player 1: ")
+            name2 = input("Please enter your name for Player 2: ")
+            self.game_add_human_player(name1)
+            self.game_add_human_player(name2)
+
+    def input_max_rounds(self):
+        max_rounds = int(input(f"Great, Hello {self.name} how many rounds would you like to play(1,3,5): "))
+        while max_rounds != 1 or max_rounds != 3 or max_rounds != 5:
+            max_rounds = int(input(f"that is not one of the options please re-enter: "))
+        self.game.set_max_rounds(max_rounds)
+
+    def get_choices(self):
+        for player in self.game.players:
+            if player.name != "Computer":
+                choice = input(f"{player.name} what move would you like to play: ")
+                while choice != "Rock" or choice != "Paper" or choice != "Lizard" or choice != "Scissors" or choice != "Spock":
+                    choice = input("Enter a move from (Rock, Paper, Lizard, Scissors, Spock")
+                player.choose_object(choice)
+            else:
+                player.choose_object()
+
+    def run_game(self):
+        while not self.game.is_finished:
+            self.get_choices()
+            print(self.game.report_round())
+            print(self.game.report_score())
+            print('Game over')
+            print(self.game.report_winner())
+
+    def run_sequence(self):
+        self.set_up()
+        print(self.game.max_rounds)
+        while running != 'n':
+            print(self.run_game())
+            running = input('do you want to run again (y/n')
 
 
+    def display_scores(self):
+        ...
+
+
+if __name__ == "__main__":
+    cli = Cli_interface()
+    cli.run_sequence()
