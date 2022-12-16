6720960b54ad7cca65216c97ceac538a6ae5c436
chanab0910 <chanab0910@highgateschool.org.uk>
2022-12-16 09:20:55 +0000

    second frame up

--- tkinter_RPSLS.py
+++ tkinter_RPSLS.py
@@ -1,30 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
-#from game_object import Game, PlayerObject, RPS_OBJECTS, RPS_WIN_DICT
+from game_object import Game, PlayerObject, RPS_OBJECTS, RPS_WIN_DICT
+from functools import partial
+
+
 
 class GUI(tk.Frame):
-    """ Test GUI subclasses the tk.Frame, so that we can use all the attributes of the tk.Frame and add our own widgets to
-    the Frame"""
-
     def __init__(self, master):
         super().__init__(master)
-
         self.Title = tk.Label(self, text='Welcome to RPSLS', font=100)
         self.Name = tk.Label(self, text='Enter your name: ')
         self.Name_input = tk.Entry(self)
-
         self.Round_num = tk.Label(self, text='Enter how many rounds you want to play: ')
-
         s = tk.StringVar()
-        values = ['1','1', '2', '3','4','5']
+        values = ['1', '1', '2', '3', '4', '5']
         self.Round_combobox = ttk.OptionMenu(self, s, *values)
-
         self.Air = tk.Label(self, text=' ')
-
         self.Quit = tk.Button(self, text='Quit', command=quit)
-
-        self.Play_Game_Button = tk.Button(self, text='Play Game')
-
+        self.Play_Game_Button = tk.Button(self, text='Play Game', command=partial(NewWindow, self))
         self.place_widgets()
 
     def place_widgets(self):
@@ -33,11 +26,21 @@ class GUI(tk.Frame):
         self.Name.grid(row=1, column=0)
         self.Name_input.grid(row=1, column=1)
         self.Round_num.grid(row=2, column=0)
-        self.Round_combobox.grid(row=2, column =1)
+        self.Round_combobox.grid(row=2, column=1)
         self.Air.grid(row=3)
         self.Quit.grid(row=4, column=0)
         self.Play_Game_Button.grid(row=4, column=1)
 
+
+class NewWindow(tk.Toplevel):
+    def __init__(self, controller):
+        super().__init__(controller)
+        self.Title = tk.Label(self, text='Welcome to RPSLS', font=100)
+        self.place_widgets()
+
+    def place_widgets(self):
+        # This code creates the widgets and grids them
+        self.Title.grid(row=0, columnspan=2)
 
 
 
@@ -48,4 +51,3 @@ if __name__ == '__main__':
     main_frame = GUI(root)
     main_frame.pack()
     root.mainloop()
-
